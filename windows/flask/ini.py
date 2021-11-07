a761cd4c96c64686a91df4917a6f867b7bb1b8d5
Andrius Pratusis <andrius.pratusis1993@gmail.com>
2021-11-07 12:25:32 +0200

    Ini change

--- windows/flask/ini.py
+++ windows/flask/ini.py
@@ -6,36 +6,38 @@ from config import admin_base_name
 from config import peekaboo_admin
 from config import peekaboo_password
 from config import db_prefix
+from plugins.connector import Base_Connector
+
 create_table_api = Blueprint("create_table_api", __name__)
 
 @create_table_api.route("/ini")
 def start_function():
     peekadmin = peekaboo_admin
     peekpassword = peekaboo_password
-    conn = sqlite3.connect(base_name+db_prefix) 
-    c = conn.cursor()
-    query = """CREATE TABLE IF NOT EXISTS {} (
+    b_a = base_name+db_prefix 
+    q_a = """CREATE TABLE IF NOT EXISTS {} (
     id integer PRIMARY KEY AUTOINCREMENT,
     urlaskey text NOT NULL,
     urlvalue text NOT NULL
     );""".format(base_name)
-    c.execute(query)
-    conn.close()
-    adminconn = sqlite3.connect(admin_base_name+db_prefix)
-    d = adminconn.cursor()
-    query = """CREATE TABLE IF NOT EXISTS {} (
+    b_c_a = Base_Connector(b_a,q_a,False,False,False)
+    b_c_a.connect_base()
+    b_b = admin_base_name+db_prefix
+    q_b = """CREATE TABLE IF NOT EXISTS {} (
     id integer PRIMARY KEY AUTOINCREMENT,
     adminname text NOT NULL,
     adminpass text NOT NULL
     );""".format(admin_base_name)
-    d.execute(query)
-    d.execute("""SELECT * FROM {}""".format(admin_base_name))
-    rows = d.fetchall()
-    query = """INSERT INTO {} (adminname,adminpass) VALUES ('{}','{}')""".format(admin_base_name,peekadmin,peekpassword)
+    b_c_b = Base_Connector(b_b,q_b,False,False,False)
+    b_c_b.connect_base()
+    q_c="""SELECT * FROM {}""".format(admin_base_name)
+    b_c_c = Base_Connector(b_b,q_c,False,True,False)
+    rows = b_c_c.connect_base()
+    
     if len(rows) == 0:
-        d.execute(query)
-        d=adminconn.commit()
-        adminconn.close()
+        q_d = """INSERT INTO {} (adminname,adminpass) VALUES ('{}','{}')""".format(admin_base_name,peekadmin,peekpassword)
+        b_c = Base_Connector(b_b,q_d,False,False,True)
+        b_c.connect_base()
     message = "Data and admin storage created !"
     message = "{}".format(message)
     return json.dumps({"status":message, "length":len(rows)}),200,{'ContentType':'application/json'}
