7f23e5988f4496fe020fc7d1374b2186f4ec3251
Andrius Pratusis <andrius.pratusis1993@gmail.com>
2021-11-11 12:01:25 +0200

    small changes

--- linux/flask/plugins/redisplugin.py
+++ linux/flask/plugins/redisplugin.py
@@ -2,12 +2,14 @@ import redis
 from config import redis_host
 from config import redis_port
 from config import redis_db
+from functools import lru_cache
 
 class Redis_Plugin:
     def __init__(self,passed_key,cache_value):
         self.passedkey = passed_key
         self.cache_value = cache_value
 
+    @lru_cache(maxsize=100)
     def return_redis_cache(self):
         r = redis.Redis(host=redis_host, port=redis_port, db=redis_db, decode_responses=True)
         if(r.exists(self.passedkey)):
